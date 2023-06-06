f18ce9800d2ce386cab87dbf4d6ff6d3d851a7d2
AmanVerma5 <118344057+AmanVerma5@users.noreply.github.com>
2023-06-06 08:57:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 //your JS code here. If required.
 window.addEventListener('resize',getWindowSize);
-
+const heading=document.createElement("h1");
 function getWindowSize() {
-	let width=document.getElementById("width");
-	let height=document.getElementById("height");
+	let width=window.innerWidth;
+	let height=window.innerHeight;
 
-	width.innerText=window.innerWidth;
-	height.innerText=window.innerHeight;
+  const div = document.getElementById('sizeInfo');
+  
+  heading.innerText = 'Width: ' + width +' and Height: ' + height;
+	div.append(heading);
 }
 getWindowSize();

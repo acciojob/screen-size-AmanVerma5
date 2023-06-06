e9ecdb785fc3ef09ef65cf1a75eb860831b4432a
AmanVerma5 <118344057+AmanVerma5@users.noreply.github.com>
2023-06-06 08:43:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+window.addEventListener('resize',getWindowSize);
+
+function getWindowSize() {
+	let width=document.getElementById("width");
+	let height=document.getElementById("height");
+
+	width.innerText=window.innerWidth;
+	height.innerText=window.innerHeight;
+}
+getWindowSize();
